3280b82d0893907179fe47e2be3801b69c5327fd
wenzhengding <wenzheng.ding@mail.utoronto.ca>
2020-09-19 23:18:39 -0400

    Added third part icon in signin page

--- uic_frontend/src/Containers/App.js
+++ uic_frontend/src/Containers/App.js
@@ -4,6 +4,7 @@ import { Provider as ReduxProvider } from "react-redux";
 import configureStore from "../Redux/store";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import SignUp from './SignUp';
+import SignIn from './SignIn';
 
 const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
 
@@ -11,14 +12,15 @@ const reduxStore = configureStore(window.REDUX_INITIAL_DATA);
 function App() {
 
     return (
-      <Router>
-          <ReduxProvider store={reduxStore}>
+      <ReduxProvider store={reduxStore}>
+          <Router>
               <div className="App">
                   <Route path='/signup' component={SignUp} />
+                  <Route path='/signin' component={SignIn} />
                   <Route path='/events' component={Uic} />
               </div>
-          </ReduxProvider>
-      </Router>
+          </Router>
+      </ReduxProvider>
   );
 }
 
